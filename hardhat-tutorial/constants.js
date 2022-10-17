074121696ec44772ed1807ecd15418f04189aecc
Lance <lance@Lances-MBP.attlocal.net>
2022-10-17 17:38:29 -0500

    Added project

---
+++ hardhat-tutorial/constants.js
@@ -0,0 +1,4 @@
+// Replace the value with your NFT contract address
+const CRYPTODEVS_NFT_CONTRACT_ADDRESS = "0x5f7B7558B59071f8622dFd541D2a46a0aE907936";
+
+module.exports = { CRYPTODEVS_NFT_CONTRACT_ADDRESS };
